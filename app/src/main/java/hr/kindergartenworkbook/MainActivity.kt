ca593e1c4fb28164c4f30700878e372bcf0eaa1c
Josip <josip.sadek1998@gmail.com>
2022-06-17 00:27:19 +0200

    -Login presentation layer is done

--- app/src/main/java/hr/kindergartenworkbook/MainActivity.kt
+++ app/src/main/java/hr/kindergartenworkbook/MainActivity.kt
@@ -2,11 +2,20 @@ package hr.kindergartenworkbook
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import hr.kindergartenworkbook.databinding.ActivityMainBinding
+import hr.kindergartenworkbook.view.LoginFragment
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        supportFragmentManager
+            .beginTransaction()
+            .add(R.id.navHostView, LoginFragment())
+            .commit()
     }
 }
--- app/src/main/java/hr/kindergartenworkbook/view/LoginFragment.kt
+++ app/src/main/java/hr/kindergartenworkbook/view/LoginFragment.kt
@@ -5,20 +5,40 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import hr.kindergartenworkbook.R
+import hr.kindergartenworkbook.databinding.FragmentLoginBinding
 
 class LoginFragment : Fragment() {
+
+    private lateinit var binding: FragmentLoginBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_login, container, false)
+    ): View {
+        binding = FragmentLoginBinding.inflate(inflater, container, false)
+        return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        binding.btnLogIn.setOnClickListener {
+            logIn()
+            switchFragment()
+        }
+    }
+
+    private fun logIn() {
+
+    }
+
+    private fun switchFragment(){
+        activity?.supportFragmentManager
+            ?.beginTransaction()
+            ?.replace(R.id.navHostView, ObservationFragment())
+            ?.addToBackStack(null)
+            ?.commit() ?: Toast.makeText(this.context, "Unable to login", Toast.LENGTH_SHORT).show()
     }
 }
